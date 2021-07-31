eab477ccffc2a7e129567cee18c3329244ad7aa1
pranav21999 <pranavdani99@gmail.com>
2021-07-31 10:02:38 +0530

    Added:Read,Write file operation using IO_File

--- src/main/java/addressBook/AddressBookTest.java
+++ src/main/java/addressBook/AddressBookTest.java
@@ -12,13 +12,35 @@ public class AddressBookTest {
 
 	@Test
 	public void whenPersonDetails_Added_ShouldReturn_True() {
-		Contact person = new Contact("pranav", "dani", "ddd", "pune", "Maharastra", "@gmail.com",
+		Contact person2 = new Contact("pranav", "dani", "ddd", "pune", "Maharastra", "@gmail.com",
 				8089987898L, 64011);
-		contact.add(person);
+		contact.add(person2);
 		System.out.println(contact.toString());
 		boolean result = addressBook.addContact(contact);
 		Assert.assertTrue(result);
 	}
+	
+	@Test
+	public void whenvalidNoOfPersonDetails_Added_ShouldReturn_false() {
+		Contact person1= new Contact("bbb", "ccc", "ddd", "delhi", "Map", "@gmail.com",
+				8089987898L, 64011);
+		contact.add(person1);
+		System.out.println(contact.toString());
+		addressBook.addContact(contact);
+ 		Assert.assertEquals(1, contact.size());
+	}
+
+	@Test
+	public void whenInvalidNoOfPersonDetails_Added_ShouldReturn_false() {
+		Contact person1= new Contact("bbb", "ccc", "ddd", "delhi", "Map", "@gmail.com",
+				8089987898L, 64011);
+		contact.add(person1);
+		System.out.println(contact.toString());
+		addressBook.addContact(contact);
+ 		Assert.assertEquals(3, contact.size());
+	}
+
+
 }
 	
 	
